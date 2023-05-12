74e5d748e7ebfa5898e12bef2ea79bff02164a96
Milan Giri <milan@teksewa.com>
2023-05-12 09:28:16 +0545

    Project structure changed

---
+++ src/ProjectFees/StateFee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjectFees
+{
+    public class StateFee
+    {
+        public string Name { get; private set; }
+        public string TwoLetterCode { get; private set; }
+        public decimal Fee { get; private set; }
+
+        public StateFee(string name, string twoLetterCode, decimal fee)
+        {
+            Name = name;
+            TwoLetterCode = twoLetterCode;
+            Fee = fee;
+        }
+    }
+}
